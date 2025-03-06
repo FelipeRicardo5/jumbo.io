1759b9b5ff7546e888da58c4cb2f83a6570176d6
Ricardo <felipe1ricardo158@gmail.com>
2025-03-06 00:51:08 -0300

    next PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from 'next-pwa';
+
+const nextConfig = nextPWA({
+  pwa: {
+    dest: 'public', // Destino do service worker (será gerado na pasta public)
+    disable: process.env.NODE_ENV === 'development',
+  },
+});
 
 export default nextConfig;
