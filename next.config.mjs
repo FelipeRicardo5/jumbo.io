07486696a4f805c0b8392f94391cc7984cb6e8ae
Ricardo <felipe1ricardo158@gmail.com>
2025-03-08 12:08:48 -0300

    adjusting Service Worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
 import nextPWA from 'next-pwa';
 
-// const nextConfig = nextPWA({
-//     pwa: {
-//       dest: 'public', // Destino do service worker (será gerado na pasta public)
-//       disable: process.env.NODE_ENV === 'development',
-//     },
-//   });
+const nextConfig = nextPWA({
+    pwa: {
+      dest: 'public', // Destino do service worker (será gerado na pasta public)
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === 'development',
+    },
+  });
 
 export default nextConfig;
